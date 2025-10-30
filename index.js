3d9307439442c554bdfb34f46b67782649fa1ef8
usert8463 <usert8463@gmail.com>
2025-10-30 20:03:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 let code = require('./utils/pair');
-let sessions = require('./utils/fonctions');
+let { sessions } = require('./utils/fonctions');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
