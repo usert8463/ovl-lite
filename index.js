1ed92b293597729330c6be0f061f2ec0da039be3
usert8463 <usert8463@gmail.com>
2025-10-31 08:32:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 let code = require('./utils/pair');
 let sessions = require('./utils/sessions');
+let sessions_dlt = require('./utils/sessions_dlt');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/code', code);
 app.use('/sessions', sessions);
+app.use('/dlt', sessions_dlt);
 
 app.use('/', (req, res) => {
-    res.sendFile('./public/pair');
+  res.sendFile(path.join(__dirname, 'public', 'pair.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+  console.log(`✅ Serveur en cours d'exécution sur http://localhost:${PORT}`);
 });
